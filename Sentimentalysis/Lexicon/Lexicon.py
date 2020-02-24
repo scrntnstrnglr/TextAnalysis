65b015268a9b75dd8cc8eef9999f926413852818
scrntnstrnglr <bhattasi@tcd.ie>
2020-02-24 10:21:18 +0000

    Added script for word-to-emtion mapping from NRC lexicon

---
+++ Sentimentalysis/Lexicon/Lexicon.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+
+"""
+
+class Lexicon:
+    def __init__(self,emo_word_file):
+        self._load_emotions(emo_word_file)
+
+    def _load_emotions(self,emo_word_file):
+        print(emo_word_file)
+
