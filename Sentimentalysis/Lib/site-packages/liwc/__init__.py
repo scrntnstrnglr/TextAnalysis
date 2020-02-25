398ff59d927bb9624da0217f4d86a7ed88840271
scrntnstrnglr <bhattasi@tcd.ie>
2020-02-25 15:33:02 +0000

    Added python library liwc

---
+++ Sentimentalysis/Lib/site-packages/liwc/__init__.py
@@ -0,0 +1,28 @@
+from .dic import read_dic
+from .trie import build_trie, search_trie
+
+try:
+    import pkg_resources
+
+    __version__ = pkg_resources.get_distribution("liwc").version
+except Exception:
+    __version__ = None
+
+
+def load_token_parser(filepath):
+    """
+    Reads a LIWC lexicon from a file in the .dic format, returning a tuple of
+    (parse, category_names), where:
+    * `parse` is a function from a token to a list of strings (potentially
+      empty) of matching categories
+    * `category_names` is a list of strings representing all LIWC categories in
+      the lexicon
+    """
+    lexicon, category_names = read_dic(filepath)
+    trie = build_trie(lexicon)
+
+    def parse_token(token):
+        for category_name in search_trie(trie, token):
+            yield category_name
+
+    return parse_token, category_names
